0040fc423dac9d4efa9e09a41d08381fc3978198
Joe Carré <carre.joe3@gmail.com>
2025-03-18 16:42:28 +0000

    setup sbom endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,33 @@
 mod requests;
 
 use requests::fetch_sbom;
+use warp::Filter;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct SbomResponse {
+    data: serde_json::Value,
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let json_data = fetch_sbom().await?;
-    println!("{:?}", json_data);
+    let sbom_route = warp::path("sbom")
+        .and(warp::get())
+        .and_then(handle_fetch_sbom);
+
+    warp::serve(sbom_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 
     Ok(())
+}
+
+async fn handle_fetch_sbom() -> Result<impl warp::Reply, warp::Rejection> {
+    match fetch_sbom().await {
+        Ok(json_data) => {
+            let response = SbomResponse { data: json_data };
+            Ok(warp::reply::json(&response))
+        }
+        Err(_) => Err(warp::reject::not_found()),
+    }
 }
