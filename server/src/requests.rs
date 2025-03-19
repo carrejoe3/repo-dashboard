a738e764614adec3a2433e015035ae92c6b2eddd
Joe Carré <carre.joe3@gmail.com>
2025-03-19 16:38:09 +0000

    Revert "setup sbom endpoint"
    This reverts commit 0040fc423dac9d4efa9e09a41d08381fc3978198.

--- server/src/requests.rs
+++ server/src/requests.rs
@@ -4,9 +4,7 @@ use std::io::Read;
 pub async fn fetch_sbom() -> Result<serde_json::Value, Box<dyn std::error::Error>> {
   let mut file = File::open("test/fixtures/example-sbom-response.json")?;
   let mut data = String::new();
-
   file.read_to_string(&mut data)?;
-
   let json: serde_json::Value = serde_json::from_str(&data)?;
   Ok(json)
 }
