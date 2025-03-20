8e5c71380fa1dbd052f740c4692e440a58e4e983
Joe Carré <carre.joe3@gmail.com>
2025-03-20 09:58:52 +0000

    split functions into separate files

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,14 @@
-mod handle_response;
+mod request_handlers;
 
+use request_handlers::requests::fetch_package_json;
 use reqwest::Error;
-use handle_response::process_success_response;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let fetch_route = warp::path!("fetch" / String / String)
-        .and_then(fetch_package_json);
+    let fetch_route = warp::path!("fetch" / String / String).and_then(fetch_package_json);
 
     warp::serve(fetch_route).run(([127, 0, 0, 1], 3030)).await;
 
     Ok(())
-}
-
-async fn fetch_package_json(owner: String, repo: String) -> Result<impl warp::Reply, warp::Rejection> {
-    let url = format!(
-        "https://raw.githubusercontent.com/{}/{}/main/package.json",
-        owner, repo
-    );
-
-    println!("Fetching package.json from: {}", url);
-
-    let response = reqwest::get(&url).await.map_err(|_| warp::reject::not_found())?;
-
-    if response.status().is_success() {
-        process_success_response(response).await.map_err(|_| warp::reject::not_found())?;
-        Ok(warp::reply::with_status("Success", warp::http::StatusCode::OK))
-    } else {
-        println!(
-            "Failed to fetch package.json from target repo: HTTP {}",
-            response.status()
-        );
-        Ok(warp::reply::with_status("Failed to fetch package.json", warp::http::StatusCode::BAD_REQUEST))
-    }
 }
--- server/src/request_handlers/handle_response.rs
+++ server/src/request_handlers/handle_response.rs
@@ -9,26 +9,26 @@ struct PackageJson {
 }
 
 pub async fn process_success_response(response: reqwest::Response) -> Result<(), Error> {
-  let package_json: PackageJson = response.json().await?;
+    let package_json: PackageJson = response.json().await?;
 
-  println!("Package.json successfully fetched!");
-  println!("{:?}", package_json);
+    println!("Package.json successfully fetched!");
+    println!("{:?}", package_json);
 
-  if let Some(dependencies) = package_json.dependencies {
-      println!("Dependencies:");
-      for (name, version) in dependencies {
-          println!("{}: {}", name, version);
-      }
-  }
+    if let Some(dependencies) = package_json.dependencies {
+        println!("Dependencies:");
+        for (name, version) in dependencies {
+            println!("{}: {}", name, version);
+        }
+    }
 
-  if let Some(dev_dependencies) = package_json.dev_dependencies {
-      println!("\nDev Dependencies:");
-      for (name, version) in dev_dependencies {
-          println!("{}: {}", name, version);
-      }
-  }
+    if let Some(dev_dependencies) = package_json.dev_dependencies {
+        println!("\nDev Dependencies:");
+        for (name, version) in dev_dependencies {
+            println!("{}: {}", name, version);
+        }
+    }
 
-  Ok(())
+    Ok(())
 }
 
 #[cfg(test)]
