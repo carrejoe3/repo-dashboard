4a076e39c9c55c7329e875ec9d605f8ddd3b6cbd
Joe Carré <carre.joe3@gmail.com>
2025-03-21 10:16:09 +0000

    Update handle_response.rs

--- server/src/request_handlers/handle_response.rs
+++ server/src/request_handlers/handle_response.rs
@@ -1,34 +1,51 @@
 use reqwest::Error;
 use serde::Deserialize;
+use serde::Serialize;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
-struct PackageJson {
-    dependencies: Option<HashMap<String, String>>,
-    dev_dependencies: Option<HashMap<String, String>>,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Repository {
+    r#type: Option<String>,
+    url: Option<String>,
 }
 
-pub async fn process_success_response(response: reqwest::Response) -> Result<(), Error> {
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PackageJson {
+    name: Option<String>,
+    version: Option<String>,
+    description: Option<String>,
+    main: Option<String>,
+    scripts: Option<HashMap<String, String>>,
+    dependencies: Option<HashMap<String, String>>,
+    devDependencies: Option<HashMap<String, String>>,
+    peerDependencies: Option<HashMap<String, String>>,
+    optionalDependencies: Option<HashMap<String, String>>,
+    author: Option<String>,
+    license: Option<String>,
+    repository: Option<Repository>,
+}
+
+pub async fn process_success_response(response: reqwest::Response) -> Result<PackageJson, Error> {
     let package_json: PackageJson = response.json().await?;
 
     println!("Package.json successfully fetched!");
     println!("{:?}", package_json);
 
-    if let Some(dependencies) = package_json.dependencies {
+    if let Some(ref dependencies) = package_json.dependencies {
         println!("Dependencies:");
         for (name, version) in dependencies {
             println!("{}: {}", name, version);
         }
     }
 
-    if let Some(dev_dependencies) = package_json.dev_dependencies {
+    if let Some(ref dev_dependencies) = package_json.devDependencies {
         println!("\nDev Dependencies:");
         for (name, version) in dev_dependencies {
             println!("{}: {}", name, version);
         }
     }
 
-    Ok(())
+    Ok(package_json)
 }
 
 #[cfg(test)]
